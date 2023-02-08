9f3f0b5f57ac8eb2e12e4d4009646c65d2acb0d4
kumanoff <mayeriegor@yandex.ru>
2023-02-08 14:41:10 +0700

    new file:   file_handler.h 	new file:   func.c 	new file:   func.o 	modified:   pipes.c 	deleted:    pipes.exe 	new file:   pipes.o 	new file:   rezult.exe

---
+++ file_handler.h
@@ -0,0 +1,2 @@
+#pragma once//defining for one time 
+void text_file(char* name_of_file);
--- pipes.c
+++ pipes.c
@@ -9,6 +9,8 @@
 #include <fcntl.h> 
 #include <sys/stat.h>
 #include <locale.h>
+#include "file_handler.h"
+void text_file(char* name); 
 
 int main()
 {
@@ -31,61 +33,30 @@ int main()
                 char file_name[100];
                 printf("type the name of file\n"); 
                 fgets(file_name, 100, stdin);
-                int n = strlen(file_name);//cause fget counting also "Enter" too
+                file_name[strlen(file_name)-1] = '\0';//
+                int n = strlen(file_name) + 1;//cause fget counting also "Enter" too
                 if (write(fd[1], &n, sizeof(int)) <0){//length of the message
                         return 2; 
                 }
-                file_name[strlen(file_name)-1] = '\0';//
-                if (write(fd[1], &file_name, strlen(file_name)) <0){//message itself 
+                if (write(fd[1], &file_name, sizeof(char)*n) <0){//
                         return 3; 
-                }
+                }  
                 close(fd[1]);
         }
         else
         {
                 /* Parent process*/
+                close(fd[1]);
                 char name_of_file[100];
                 int n;
-                int inputFd, outputFd, numRead, total, openFlags;
-                mode_t filePerms;
-
-                close(fd[1]);
                 if (read(fd[0], &n, sizeof(int))<0){
                         return 4;
                 }
                 if (read(fd[0], &name_of_file, sizeof(char)*n) <0){
                         return 5;
                 }
-                for (int i=0; i<strlen(name_of_file);i++){
-                        printf("%d  ", name_of_file[i]);
-                }
-                printf("\n");
 
-                inputFd = open(name_of_file, O_RDONLY);
-                if (inputFd == -1) {
-                    printf("Error opening file: %s\n", name_of_file);
-                    exit(-2);
-                }
-                openFlags = O_CREAT | O_WRONLY | O_TRUNC;
-                filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
-                char output_name[]="out.txt";
-                outputFd = open(output_name, openFlags, filePerms);
-                char buf;
-                total = 0;
-                // цикл для обработки файла
-                while ((numRead = read(inputFd, &buf, 1) > 0))
-                   {
-                    write(outputFd, &buf, numRead);
-                    if (buf == '.') {
-                       buf = 0x0a;
-                       write(outputFd, &buf, numRead);
-                       total++;
-                    }
-                }
-                printf("Amount of operations: %d\n", total);
-                close(inputFd);
-                close(outputFd);
-
+                text_file(name_of_file); 
                 close(fd[0]);
                 wait(NULL); 
         }
